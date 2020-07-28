4371e8adf7bc3641801fa07a0b1b5af3b23cc913
Wendel <wendelrioss@hotmail.com>
2020-07-27 21:52:47 -0300

    react native first module done

--- nivel01/mobileReactNative/src/index.js
+++ nivel01/mobileReactNative/src/index.js
@@ -1,13 +1,70 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet, StatusBar, FlatList, SafeAreaView, TouchableOpacity} from 'react-native';
+import api from './services/api';
 
-export default function App(){
-  return <View style={styles.container}/>;
+function App() {
+
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      console.log(response.data)
+      setProjects(response.data)
+    })
+  },[])
+
+  async function handleAddProject(){
+    const response = await api.post('/repositories', {
+      title:`Novo projeto ${Date.now()}`,
+      owner:'Wendel Rios'
+    })
+    const project = response.data
+    setProjects([...projects,project])
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({ item: project}) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+        <TouchableOpacity activeOpacity={0.7} style={styles.button} onPress={handleAddProject}>
+          <Text style={styles.buttonText}>Adicionar Projeto</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:'#7159c1',
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
   },
+  project: {
+    color: '#FFF',
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  button:{
+    backgroundColor:'#FFF',
+    margin:20,
+    height:50,
+    borderRadius:4,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  buttonText:{
+    fontWeight:'bold',
+    fontSize:16 
+  }
 });
+
+export default App;
